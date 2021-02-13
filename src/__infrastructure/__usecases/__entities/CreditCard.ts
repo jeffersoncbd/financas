b9828834fd06014e9036b1ac6630472d7817b4e7
Jefferson Carlos <jeff.carlosbd@gmail.com>
2021-02-13 14:49:37 -0300

    feat: faz entidade cartao de credito gerar o proprio ID

--- src/__infrastructure/__usecases/__entities/CreditCard.ts
+++ src/__infrastructure/__usecases/__entities/CreditCard.ts
@@ -1,6 +1,24 @@
-export interface CreditCard {
+import * as uuid from 'uuid'
+
+abstract class CreditCardFields {
   name: string
   closingDay: number
   dueDay: number
 }
 
+interface CreditCardData extends CreditCardFields {
+  id?: string
+}
+
+export class CreditCard extends CreditCardFields {
+  readonly id: string
+
+  constructor(data: CreditCardData) {
+    super()
+    this.id = data.id || uuid.v4()
+
+    this.name = data.name
+    this.closingDay = data.closingDay
+    this.dueDay = data.dueDay
+  }
+}
